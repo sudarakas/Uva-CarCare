0eef2a540b11aeb9fe20d10ef5736b5b00d0988c
randika <randikamadu1995@gmail.com>
2018-11-18 20:21:14 +0530

    Updated Database Tables

---
+++ database/uva_car_care.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2018 at 02:48 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uva_car_care`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appoinment`
+--
+
+CREATE TABLE `appoinment` (
+  `InvoiceNo` varchar(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `PhoneNo` int(10) NOT NULL,
+  `Service` varchar(30) NOT NULL,
+  `ServiceCenter` varchar(30) NOT NULL,
+  `Date` date NOT NULL,
+  `Time` date NOT NULL,
+  `Message` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `InvoiceNo` varchar(30) NOT NULL,
+  `BillID` varchar(30) NOT NULL,
+  `Amount` decimal(10,0) NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE `job` (
+  `InvoiceNo` varchar(30) NOT NULL,
+  `EmpName` varchar(30) NOT NULL,
+  `Status` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserType` varchar(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Gender` varchar(5) NOT NULL,
+  `PhoneNo` int(10) NOT NULL,
+  `NIC` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Password` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
